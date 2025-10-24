78ed49b84bd38985477a4f4428b95b959cc648d4
ninzzd <ninaad.desai1212@gmail.com>
2025-10-24 20:46:26 +0530

    Generated the .mif file successfully

--- tb/mifgen.c
+++ tb/mifgen.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 /*
-    assign mask_a = 32'h8020_0003;
-    assign seed_a = 32'h0000_0001;
-    assign mask_b = 32'h8000_0063;
-    assign seed_b = 32'hDEAD_BEEF;
+    mask_a = 32'h8020_0003;
+    seed_a = 32'h0000_0001;
+    mask_b = 32'h8000_0063;
+    seed_b = 32'hDEAD_BEEF;
 */
 int lfsr32(unsigned int seed, unsigned int mask, unsigned int n, unsigned int* outputs){
     outputs[0] = seed;
@@ -19,20 +19,41 @@ int lfsr32(unsigned int seed, unsigned int mask, unsigned int n, unsigned int* o
     }
     return 0;
 }
+/*
+.mif file example:
+DEPTH = 4096;
+WIDTH = 64;
+ADDRESS_RADIX = HEX;
+DATA_RADIX = HEX;
+
+CONTENT BEGIN
+    000 : 0000_0000_0000_0000;
+    001 : 0000_0000_0000_0001;
+    [... many more entries ...]
+    FFF : FFFF_FFFF_FFFF_FFFF;
+END;
+*/
 int main(){
-    int n;
-    scanf("%d",&n);
+    FILE *f;
+    int n = 16;
+    int m = n/2;
+    f = fopen("res.mif","w");
+    fprintf(f,"DEPTH = %d;\n",n);
+    fprintf(f,"WIDTH = 64;\n");
+    fprintf(f,"ADDRESS_RADIX = HEX;\n");
+    fprintf(f,"DATA_RADIX = HEX;\n");
+    fprintf(f,"CONTENT BEGIN\n");
     unsigned int* lfsr_a = (unsigned int*)malloc(n*sizeof(unsigned int));
     unsigned int* lfsr_b = (unsigned int*)malloc(n*sizeof(unsigned int));
-    lfsr32(0x00000001,0x80200003,n,lfsr_a);
-    lfsr32(0xDEADBEEF,0x80000063,n,lfsr_b);
-    printf("LFSR A:\n");
-    for(int i = 0;i < n;i++){
-        printf("[%d]: 0x%X = %u\n",i,lfsr_a[i],lfsr_a[i]);
+    lfsr32(0x00000001,0x80200003,m,lfsr_a);
+    lfsr32(0xDEADBEEF,0x80000063,m,lfsr_b);
+    for(int i = 0;i < m;i++){
+        int64_t res = (int64_t)(int)lfsr_a[i]*(int64_t)(int)lfsr_b[i];
+        uint64_t resu = (uint64_t)lfsr_a[i]*(uint64_t)lfsr_b[i];
+        fprintf(f,"\t%01X : %016lX\n",(unsigned int)(2*i),resu);
+        fprintf(f,"\t%01X : %016lX\n",(unsigned int)(2*i+1),res);
     }
-    printf("LFSR B:\n");
-    for(int i = 0;i < n;i++){
-        printf("[%d]: 0x%X = %u\n",i,lfsr_b[i],lfsr_b[i]);
-    }
+    fprintf(f,"END\n");
+    fclose(f);
     return 0;
 }
