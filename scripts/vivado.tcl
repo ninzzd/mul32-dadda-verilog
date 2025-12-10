08d7f762fa7cadee033ad007a1d08bf823781ffb
ninzzd <ninaad.desai1212@gmail.com>
2025-12-10 16:46:52 +0530

    TCL not working Added vivado project and log files into git ignore

--- scripts/vivado.tcl
+++ scripts/vivado.tcl
@@ -1,41 +1,53 @@
-# anchor everything to the directory containing this TCL script
+# --- 1. Directory and Project Setup ---
+# Anchor the working directory to the parent of the script (e.g., project root)
 set cwd [file normalize [file join [file dirname [info script]] ".."]]
 cd $cwd
-# puts "Current working directory: $cwd"
-# exit
-# Set project variables
+
+# Define project name and directory
 set proj_name "mul32-dadda-verilog"
 set proj_dir  [file normalize [file join $cwd "${proj_name}-vivado"]]
-set proj_xpr  [file normalize [file join $proj_dir "${proj_name}.xpr"]]
+set part_name "xc7a100tcsg324-1"
+set board_part "digilentinc.com:nexys-a7-100t:part0:1.2"
 
+# Check if project exists, open it if it does.
+set proj_xpr  [file normalize [file join $proj_dir "${proj_name}.xpr"]]
 if {[file exists $proj_xpr]} {
+    puts "INFO: Opening existing project: $proj_xpr"
     open_project $proj_xpr
 } else {
-    file mkdir $proj_dir
-    cd $proj_dir
-    exec git clone https://github.com/Digilent/vivado-boards
-    create_project $proj_name $proj_dir -part xc7a100tcsg324-1
+    puts "INFO: Creating new project in: $proj_dir"
+    
+    # Create the project
+    create_project $proj_name $proj_dir -part $part_name
+    
+    # Set the board property now that it's manually installed
+    set_property board_part $board_part_name [current_project]
 
-    # Add design sources from the src/ directory (relative to script)
+    # --- 2. Add Design Sources (src/) ---
     set src_dir [file join $cwd "src"]
-    foreach f [glob -nocomplain -directory $src_dir *.v] {
-        add_files -fileset sources_1 $f
+    if {[file isdirectory $src_dir]} {
+        puts "INFO: Adding design sources from $src_dir"
+        foreach f [glob -nocomplain -directory $src_dir *.v] {
+            add_files -fileset sources_1 $f
+        }
+    } else {
+        puts "WARNING: Source directory '$src_dir' not found."
     }
 
-    # Add simulation sources from the tb/ directory (relative to script)
+    # --- 3. Add Simulation Sources (tb/) ---
     set tb_dir [file join $cwd "tb"]
-    foreach f [glob -nocomplain -directory $tb_dir *.v] {
-        add_files -fileset sim_1 $f
+    if {[file isdirectory $tb_dir]} {
+        puts "INFO: Adding simulation sources from $tb_dir"
+        foreach f [glob -nocomplain -directory $tb_dir *.v] {
+            add_files -fileset sim_1 $f
+        }
+    } else {
+        puts "WARNING: Testbench directory '$tb_dir' not found."
     }
-
-    # Set the target board to Nexys A7
-    # Platform: Nexys A7
-    set bd_path [file normalize [file join $proj_dir "vivado-boards" "new" "board_files"]]
-    set_property board.repo_paths [list $bd_path] [current_project]
-    set_property board_part xilinx.com:nexys-a7-100:part0:1.0 [current_project]
 
     # Save the project
     save_project
 }
 
+# Start the Vivado GUI
 start_gui
